77db5225637e01a878fe6cf75110302d7fc20ff2
Francis M Schappo <francismallmann@gmail.com>
2015-04-20 23:11:12 -0300

    mais mudanças

--- Prolog/t4-assassinato.pl
+++ Prolog/t4-assassinato.pl
@@ -58,3 +58,53 @@ ferramenta(martelo).
 %Insanos
 insano(adriano).
 insano(maria).
+
+%Fatos
+fato(pedro, santa_maria, segunda).
+fato(pedro, santa_maria, terca).
+fato(pedro, porto_alegre, quarta).
+fato(pedro, santa_maria, quinta).
+fato(pedro, apartamento, sexta).
+
+fato(caren, porto_alegre, segunda).
+fato(caren, porto_alegre, terca).
+fato(caren, porto_alegre, quarta).
+fato(caren, santa_maria, quinta).
+fato(caren, apartamento, sexta).
+
+fato(henrique, apartamento, segunda).
+fato(henrique, porto_alegre, terca).
+fato(henrique, apartamento, quarta).
+fato(henrique, apartamento, quinta).
+fato(henrique, apartamento, sexta).
+
+fato(bia, apartamento, segunda).
+fato(bia, porto_alegre, terca).
+fato(bia, porto_alegre, quarta).
+fato(bia, santa_maria, quinta).
+fato(bia, apartamento, sexta).
+
+fato(adriano, apartamento, segunda).
+fato(adriano, apartamento, terca).
+fato(adriano, santa_maria, quarta).
+fato(adriano, apartamento, quinta).
+fato(adriano, apartamento, sexta).
+
+fato(alice, apartamento, segunda).
+fato(alice, porto_alegre, terca).
+fato(alice, porto_alegre, quarta).
+fato(alice, apartamento, quinta).
+fato(alice, apartamento, sexta).
+
+fato(bernardo, santa_maria, segunda).
+fato(bernardo, santa_maria, terca).
+fato(bernardo, porto_alegre, quarta).
+fato(bernardo, santa_maria, quinta).
+fato(bernardo, apartamento, sexta).
+
+fato(maria, apartamento, segunda).
+fato(maria, santa_maria, terca).
+fato(maria, santa_maria, quarta).
+fato(maria, santa_maria, quinta).
+fato(maria, apartamento, sexta).
+
