0eba627f568dadc53891df8f542daec1043d5de2
Francis M Schappo <francismallmann@gmail.com>
2015-04-20 22:29:30 -0300

    T4 Prolog

---
+++ Prolog/t4-assassinato.pl
@@ -0,0 +1,22 @@
+%Trabalho 4 - Prolog
+%Nome: Francis Mallmann Schappo
+%Matricula: 201021524
+
+%Condição financeira
+
+
+
+
+%Relacionamentos dos envolvidos
+
+
+
+%Motivos de Crime
+motivo(dinheiro).
+motivo(ciumes).
+motivo(insanidade).
+
+
+%Ferramentas para o Crime
+ferramenta(bastao).
+ferramenta(martelo).
